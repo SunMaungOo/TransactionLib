9ac35ad5ed16f4feb490758b8ec832d381670c56
Sun Maung Oo <2162432+SunMaungOo@users.noreply.github.com>
2018-12-21 22:36:00 +0630

    Add project files.

---
+++ TransactionLib/TransactionManager.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TransactionLib
+{
+    /// <summary>
+    /// When we wanted to link multiple method in a transaction pattern
+    /// Author = Sun Maung Oo
+    /// </summary>
+    public sealed class TransactionManager : IDisposable
+    {
+        private bool isCommited;
+
+        private bool isDisposed;
+
+        private IList<TransactionWrapper> wrapperList;
+
+        /// <summary>
+        /// Should only be created in using(...) statement
+        /// </summary>
+        public TransactionManager()
+        {
+
+            isCommited = false;
+
+            isDisposed = false;
+
+            wrapperList = new List<TransactionWrapper>();
+
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="method"></param>
+        /// <param name="rollbackMethod"></param>
+        public void Create(Action method, Action rollbackMethod)
+        {
+            try
+            {
+                wrapperList.Add(new TransactionWrapper(method, rollbackMethod));
+            }
+            catch
+            {
+
+                Dispose();
+
+                //rethrow whatever the exception is caught
+
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Commit an method.
+        /// Provide guarantee that the method success if you can call this method.
+        /// In other word the program will not execute this method line number if the 
+        /// exception occur.
+        /// </summary>
+        public void Commit()
+        {
+            isCommited = true;
+
+            foreach (TransactionWrapper wrapper in wrapperList)
+            {
+                wrapper.Commit();
+            }
+        }
+
+        /// <summary>
+        /// Warning : Dispose() method may only call in using(...) statement in order
+        /// to provide a transaction.
+        /// 
+        /// Calling Dispose() method other than in using(...) statement break the 
+        /// transaction pattern.
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            if (!isDisposed)
+            {
+                isDisposed = true;
+
+                if (!isCommited)
+                {
+                    foreach (TransactionWrapper wrapper in wrapperList)
+                    {
+                        wrapper.Dispose();
+                    }
+                }
+
+                wrapperList.Clear();
+            }
+        }
+
+
+    }
+}
---
+++ TransactionLib/TransactionWrapper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace TransactionLib
+{
+    /// <summary>
+    /// When we want an transaction pattern for single method.
+    /// If the exception is thrown, the rollback method is automatically called
+    /// Author = Sun Maung Oo
+    /// </summary>
+    public sealed class TransactionWrapper : IDisposable
+    {
+        private Action rollbackMethod;
+
+        private bool isCommited;
+
+        private bool isDisposed;
+
+        /// <summary>
+        /// Should only be created in using(...) statement
+        /// </summary>
+        /// <param name="method">Method to execute</param>
+        /// <param name="rollbackMethod"> Method which will execute to rollback the change provided </param>
+        public TransactionWrapper(Action method, Action rollbackMethod)
+        {
+            this.rollbackMethod = rollbackMethod;
+
+            isCommited = false;
+
+            isDisposed = false;
+
+            try
+            {
+                method();
+            }
+            catch
+            {
+                Dispose();
+
+                //rethrow whatever the exception is caught
+
+                throw;
+            }
+        }
+
+
+        /// <summary>
+        /// Commit an method.
+        /// Provide guarantee that the method success if you can call this method.
+        /// In other word the program will not execute this method line number if the 
+        /// exception occur.
+        /// </summary>
+        public void Commit()
+        {
+            isCommited = true;
+        }
+
+        /// <summary>
+        /// Warning : Dispose() method may only call in using(...) statement in order
+        /// to provide a transaction.
+        /// 
+        /// Calling Dispose() method other than in using(...) statement break the 
+        /// transaction pattern.
+        /// 
+        /// </summary>
+        public void Dispose()
+        {
+            //if it haven't been dispose yet
+
+            if (!isDisposed)
+            {
+                isDisposed = true;
+
+                if (!isCommited && rollbackMethod != null)
+                {
+                    rollbackMethod();
+                }
+            }
+
+        }
+
+    }
+}
